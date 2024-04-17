3518e77a5fbe257d4d7fac3ecfc4c37168891751
VincentXiao <xiaoyuchen58@outlook.com>
2024-04-17 15:52:23 +0800

    different ways to declare and assign variables.

---
+++ VARIABLES/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// one way - decare, then assign (two steps)
+	var firstNumber int
+	firstNumber = 2
+
+	// another way - declare and assign (one step)
+	var secondNumber = 5
+
+	// one step varaible: assign and declare
+	subtraction := 7
+}
