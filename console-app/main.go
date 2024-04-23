39012051c1763a69d93711dc836bce9fbf53e14f
VincentXiao <xiaoyuchen58@outlook.com>
2024-04-23 13:57:21 +0800

    console app update.

--- console-app/main.go
+++ console-app/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
+	"log"
+
+	"github.com/eiannone/keyboard"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("-> ")
-		userInput, _ := reader.ReadString('\n')
-		userInput = strings.Replace(userInput, "\n", "", -1)
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err);
+	}
 
-		if userInput == "exit" {
-			break
-		} else {
-			fmt.Println("You entered:", userInput)
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	fmt.Println("Press any key, ESC to quit")
+
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			log.Fatal(err)
 		}
+		if key == keyboard.KeyEsc {
+			break
+		}
+
+		fmt.Printf("You pressed: rune %q, key %X\r\n", char, key)
 	}
 }
