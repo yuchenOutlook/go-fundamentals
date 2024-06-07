3c09da28af930e240cd51630008d3897a7daba24
VincentXiao <xiaoyuchen58@outlook.com>
2024-06-07 14:32:14 +0800

    feat: Add closing channels example
    The code changes in `closing-channels.go` add an example of closing channels in Go. This demonstrates how to indicate that no more values will be sent on a channel. The example includes a goroutine that receives jobs from the channel and prints them until the channel is closed. The main function sends three jobs to the channel and then closes it. Finally, the main function waits for the goroutine to finish by receiving a value from the `done` channel.

---
+++ go-by-examples/35-Closing-Channels/closing-channels.go
@@ -0,0 +1,39 @@
+package main
+
+// https://gobyexample.com/closing-channels
+// Closing a channel indicates that no more values will be sent on it.
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		time.Sleep(1 * time.Second)
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<-done
+
+	_, ok := <-jobs
+	fmt.Println("Received more jobs:", ok)
+}
