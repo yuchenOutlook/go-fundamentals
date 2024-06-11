e9bb125808d5fdcdc2d550711d8057485d70cd30
VincentXiao <xiaoyuchen58@outlook.com>
2024-06-11 15:41:35 +0800

    feat: Add buffered channel example
    The code changes in `non-blocking-channel-operations.go` demonstrate how to use a buffered channel to allow sending a message even when there is no receiver. This improves the flexibility of the program by preventing blocking when the channel has no buffer and there is no receiver.

--- go-by-examples/34-Non-blocking-channel-operations/non-blocking-channel-operations.go
+++ go-by-examples/34-Non-blocking-channel-operations/non-blocking-channel-operations.go
@@ -16,9 +16,9 @@ func main() {
 	}
 
 	/*
-	A non-blocking send works similarly. 
-	Here msg cannot be sent to the messages channel, 
-	because the channel has no buffer and there is no receiver. 
+	A non-blocking send works similarly.
+	Here msg cannot be sent to the messages channel,
+	because the channel has no buffer and there is no receiver.
 	Therefore the default case is selected.
 	*/
 	msg := "hi"
@@ -37,5 +37,41 @@ func main() {
 	default:
 		fmt.Println("no activity")
 	}
-	
+
+}
+
+/*
+Version 2: If you want the message to be sent to the channel, you can make the channel buffered.
+*/
+package main
+
+import "fmt"
+
+func main() {
+    messages := make(chan string, 1)  // Make a buffered channel
+    signals := make(chan bool)
+
+    select {
+    case msg := <-messages:
+        fmt.Println("received message", msg)
+    default:
+        fmt.Println("no message received")
+    }
+
+    msg := "sample message"
+    select {
+    case messages <- msg:
+        fmt.Println("sent message", msg)
+    default:
+        fmt.Println("no message sent")
+    }
+
+    select {
+    case msg := <-messages:
+        fmt.Println("received message", msg)
+    case sig := <-signals:
+        fmt.Println("received signal", sig)
+    default:
+        fmt.Println("no activity")
+    }
 }
