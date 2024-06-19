16d23fb7f09588d548e4f9f8713e9190d779d54a
VincentXiao <xiaoyuchen58@outlook.com>
2024-06-19 16:55:35 +0800

    feat: add panic and defer examples.

---
+++ go-by-examples/48-Defer/defer-example.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f := createFile("/tmp/defer.txt")
+	// defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("writing")
+	os.WriteFile(f.Name(), []byte("hello world\n"), os.FileMode(0644))
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
