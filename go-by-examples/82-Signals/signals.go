de7eba097fbc232704238e152f9e64c902f85a36
VincentXiao <xiaoyuchen58@outlook.com>
2024-07-11 09:56:26 +0800

    feat: spawning processes, executing processes from go, receiveing signals, exit program examples.

---
+++ go-by-examples/82-Signals/signals.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGTSTP, syscall.SIGQUIT)
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <- sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
